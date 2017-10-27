8714498d7a93a0fe818f958968391600f2c5dcdd
jotaro <>
2017-10-27 21:46:46 +0900

    initial commit

--- init.el
+++ init.el
@@ -5,11 +5,26 @@
         ("melpa" . "http://melpa.org/packages/")
         ("org" . "http://orgmode.org/elpa/")))
 
+;;load-path add
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
 ;; バックスペースの設定
 (global-set-key (kbd "C-h") 'delete-backward-char)
 
+;;C-i はタブじゃなくてC-iにする
+(define-key input-decode-map "\C-i" [C-i])
+
+;; switch window
+(global-set-key [C-tab] 'other-window)
+
+;; auto-complete（自動補完）
+(require 'auto-complete)
+(require 'auto-complete-config)
+(global-auto-complete-mode 0.25)
+
 ;; font
-(add-to-list 'default-frame-alist '(font . "Source Code Pro-12"))
+;;(add-to-list 'default-frame-alist '(font . "Source Code Pro-11"))
+(add-to-list 'default-frame-alist '(font . "Inconsolata-g for Power Line-9"))
 
 ;; color theme
 (load-theme 'monokai t)
@@ -30,6 +45,11 @@
 
 ;; tabサイズ
 (setq default-tab-width 4)
+
+;;Find-file in project
+;; (require 'find-file-in-project)
+;; (global-set-key "\C-i" 'find-file-in-project)
+
 
 ;; メニューバーを非表示
 (menu-bar-mode 0)
@@ -52,14 +72,75 @@
 ;; タイトルにフルパス表示
 (setq frame-title-format "%f")
 
-;;current directory 表示
-(let ((ls (member 'mode-line-buffer-identification
-                  mode-line-format)))
-  (setcdr ls
-    (cons '(:eval (concat " ("
-            (abbreviate-file-name default-directory)
-            ")"))
-		  (cdr ls))))
+
+;; Ivy settei
+(ivy-mode 1)
+
+;;; 下記は任意で有効化
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-load-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-u") 'counsel-projectile)
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
+
+;; ;; helm設定
+;; (require 'helm)
+;; (require 'helm-config)
+
+;; ;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; ;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; ;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+;; (global-set-key (kbd "C-c h") 'helm-command-prefix)
+;; (global-unset-key (kbd "C-x c"))
+
+;; (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+;; (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
+;; (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+;; (when (executable-find "curl")
+;;   (setq helm-google-suggest-use-curl-p t))
+
+;; (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+;;       helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+;;       helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+;;       helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+;;       helm-ff-file-name-history-use-recentf t
+;;       helm-echo-input-in-header-line t)
+
+;; (defun spacemacs//helm-hide-minibuffer-maybe ()
+;;   "Hide minibuffer in Helm session if we use the header line as input field."
+;;   (when (with-helm-buffer helm-echo-input-in-header-line)
+;;     (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+;;       (overlay-put ov 'window (selected-window))
+;;       (overlay-put ov 'face
+;;                    (let ((bg-color (face-background 'default nil)))
+;;                      `(:background ,bg-color :foreground ,bg-color)))
+;;       (setq-local cursor-type nil))))
+
+
+;; (add-hook 'helm-minibuffer-set-up-hook
+;;           'spacemacs//helm-hide-minibuffer-maybe)
+
+;; (setq helm-autoresize-max-height 0)
+;; (setq helm-autoresize-min-height 20)
+;; (helm-autoresize-mode 1)
+
+;; (helm-mode 1)
+
+;;counsel-projectil
+(counsel-projectile-on)
 
 
 ;; スタートアップメッセージを表示させない
@@ -141,7 +222,7 @@
 	("98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" default)))
  '(package-selected-packages
    (quote
-	(dracula-theme material-theme powerline glsl-mode git-gutter neotree monokai-theme hiwin elscreen auto-complete))))
+	(markdown-mode counsel-projectile projectile shell-here counsel-ebdb shell-pop ivy helm dracula-theme material-theme powerline glsl-mode git-gutter neotree monokai-theme hiwin elscreen auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -151,7 +232,7 @@
 
 
 ;; スクロールは1行ごとに
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 5)))
+(setq mouse-wheel-scroll-amount '(3 ((shift) . 5)))
 
 ;; スクロールの加速をやめる
 (setq mouse-wheel-progressive-speed nil)
@@ -170,3 +251,49 @@
 ;; Powerlineをつかう
 (require 'powerline)
 (powerline-default-theme)
+
+
+;; smoke test
+;; eshell + shell-pop
+;;(setq shell-pop-shell-type '("eshell" "*eshell*" (lambda () (eshell))))
+(setq shell-pop-shell-type '("ansi-term" "*ansi-term" (lambda () (ansi-term shell-pop-term-shell))))
+(setq shell-pop-window-size 20)
+
+  
+(global-set-key (kbd "C-c o") 'shell-pop)
+
+(display-time-mode 1)
+
+
+;; case insensitive
+(setq case-fold-search t)
+
+;; find-name-dired
+(define-key global-map [(super i)] 'find-name-dired)
+
+;; grep
+(define-key global-map [(super f)] 'counsel-rg)
+;; (define-key global-map [(super shift o)] 'counsel-git-grep)
+(define-key global-map [(super shift o)] 'counsel-rg)
+
+;; rgrepのheader messageを消去
+(defun delete-grep-header ()
+  (save-excursion
+    (with-current-buffer grep-last-buffer
+      (forward-line 5)
+      (narrow-to-region (point) (point-max)))))
+(defadvice grep (after delete-grep-header activate) (delete-grep-header))
+(defadvice rgrep (after delete-grep-header activate) (delete-grep-header))
+
+;; rgrep時などに，新規にwindowを立ち上げる
+(setq special-display-buffer-names '("*Help*" "*compilation*" "*interpretation*" "*grep*"))
+;; (add-hook 'compilation-finish-functions
+;;            (lambda (buf strg)
+;;              (let ((win  (get-buffer-window buf 'visible)))
+;;                (when win (delete-window win)))))
+(setq special-display-frame-alist
+      (append (list
+			   '(width . 100)
+			   '(height . 50)
+			   )
+			  special-display-frame-alist))
